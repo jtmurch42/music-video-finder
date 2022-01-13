751470462718f183bf1f2e2363260770bacee74b
Jeremy Murchison <jtmurch92@gmail.com>
2022-01-12 19:31:59 -0700

    Rename app. Package updates.

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage/ng-music-videos"),
+      dir: require("path").join(__dirname, "./coverage/ng-music-video-finder"),
       subdir: ".",
       reporters: [{ type: "html" }, { type: "text-summary" }]
     },
